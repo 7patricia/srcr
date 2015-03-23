d00148b97dff825335a32d2724b73ccbc0c6404d
Patrícia <bpatrcia@gmail.com>
2015-03-23 22:33:47 +0000

    Added muita coisa
    Quase tudo a funcionar. Falta adicionarbisavos, trisavos, tetravos.
    ascendenteAte, ascendentesAte, descendentesAte não funciona. tios,
    primos, irmaos, nao funciona.

--- Exercício1.pl
+++ Exercício1.pl
@@ -68,14 +68,32 @@ teste([R|LR]) :-
 	R,
 	teste(LR).
 
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensão do predicado que permite a remoção do conhecimento
+
+remocao(Termo) :- retract(Termo).
+
 
 %---------------------------------------------------------------------
 % INVARIANTES
 
 
-% INVARIANTES ESTRUTURAIS - NÃO PERMITEM INSERÇÃO DE CONHECIMENTO apagaRepetidos
+% INVARIANTES ESTRUTURAIS - NÃO PERMITEM INSERÇÃO DE CONHECIMENTO 
+
++natural(I,L) :: (findall( (I,L),(natural( I,L )),S ),
+                  comprimento( S,N ), 
+				  N == 1
+                  ).
 
 +filho(F,P) :: (findall( (F,P),(filho( F,P )),S ),
+                  comprimento( S,N ), 
+				  N == 1
+                  ).
++avo(A,N) :: (findall( (A,N),(avo( A,N )),S ),
+                  comprimento( S,N ), 
+				  N == 1
+                  ).
++neto(N,A) :: (findall( (N,A),(neto( N,A )),S ),
                   comprimento( S,N ), 
 				  N == 1
                   ).
@@ -106,6 +124,13 @@ teste([R|LR]) :-
 %--------------------------------------------------------------------
 % INVARIANTES REFERENCIAIS
 
+%--- INVARIANTE QUE NÃO PERMITE UM INDIVIDUO SER NATURAL DE MAIS DO QUE UM LOCALIDADE
+
++natural(I,N) :: (findall((Ns),(natural(I,Ns)),S),
+				comprimento(S,X),
+				X=<2
+				).
+
 %--- INVARIANTES QUE NÃO PERMITEM MAIS DO QUE 2 PROGENITORES PARA O MESMO INDIVIDUO
 
 +filho( F,P ) :: ( findall((Ps),(filho(F,Ps)),S),
@@ -115,6 +140,17 @@ teste([R|LR]) :-
 +pai(P,F) :: (findall((Ps),(pai(Ps,F)),S),
 			comprimento(S,X),
 			X =<2
+			).
+%----- INVARIANTES QUE NÃO PERMITEM MAIS DO QUE 4 AVOS PARA O MESMO INDIVIDUO
+
++neto(N,A) :: (findall((As),(neto(N,As)),S),
+			comprimento(S,X),
+			X=<4
+			).
+
++avo(A,N) :: (findall((As),(avo(As,N)),S),
+			comprimento(S,X),
+			X=<4
 			).
 
 %---- INVARIANTE QUE NÃO PERMITE QUE O FILHO SEJA PAI DO PAI
@@ -132,6 +168,9 @@ teste([R|LR]) :-
 %---- INVARIANTE QUE NÃO PERMITE QUE O SOBRINHO SEJA PAI DO TIO
 +pai(P,F) :: nao(sobrinho(P,F)).
 
+%---- INVARIANTE QUE NÃO PERMITE QUE O AVO SEJA PAI DO NETO
++pai(P,F) :: nao(avo(P,F)).
+
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado filho: Filho,Pai -> {V,F}
@@ -146,16 +185,31 @@ filho(rui,isabel).
 filho(rui,joaquim).
 
 filho(tiago,suzana).
-filho(tiago,manuel).
+filho(tiago,ze).
 
 filho(mariana,suzana).
-filho(mariana,manuel).
+filho(mariana,ze).
 
 filho(matias,suzana).
-filho(matias,manuel).
+filho(matias,ze).
 
 filho(pedro,carlos).
 filho(pedro,sameiro).
+
+filho(olga,maria).
+filho(olga,antonio).
+
+filho(carlos,maria).
+filho(carlos,antonio).
+
+filho(isabel,maria).
+filho(isabel,antonio).
+
+filho(luis,argentina).
+filho(luis,manuel).
+
+filho(ze,argentina).
+filho(ze,manuel).
 
 %--------------------------------------------------------------------
 % Extensao do predicado natural : Individuo,Localidade -> {V,F}
@@ -179,11 +233,40 @@ natural(pedro,arcos).
 natural(sameiro,arcos).
 natural(carlos,arcos).
 
+natural(argentina,arcos).
+natural(manuel,arcos).
+
+natural(antonio,tavora).
+natural(maria,tavora).
+
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado filhos: Pai,Resultados -> {V,F}
 
 filhos(I, R) :-
 findall(P, filho(P, I), S),
+R = S .
+
+% Extensao do predicado avo: Avo,Neto -> {V,F}
+
+avo( A,N ) :- filho( N,X ) , filho( X,A ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado avos: Neto,Resultados -> {V,F}
+
+avos(I, R) :-
+findall(P, avo(P, I), S),
+R = S .
+
+%---------------------------------------------------------------------
+% Extensão do predicado neto : Neto,Avo -> {V,F}
+
+neto(N,A) :- avo(A,N).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado netos: Avo,Resultados -> {V,F}
+
+netos(I, R) :-
+findall(P, neto(P, I), S),
 R = S .
 
 %----------------------------------------------------
@@ -264,6 +347,23 @@ descendente( X,Y ) :-
 	filho( X,A ),
 	descendente( A,Y ).	
 
+%--------------------------------------------------------------------
+% Extensão do predicado descendentes: Ascendente, Resultado -> {V,F}
+
+descendentes(I,R) :- findall(D,descendente(D,I),S),
+					R = S.
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado ascendente: Ascendente, Descendente -> {V,F}
+ascendente( X,Y ) :- descendente( Y,X ).
+
+
+%--------------------------------------------------------------------
+% Extensão do predicado ascendentes: Descendente, Resultado -> {V,F}
+
+ascendentes(I,R) :- findall(D,ascendente(D,I),S),
+					R = S.
+
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Extensao do predicado descendente: Descendente,Ascendente,Grau -> {V,F}
 descendente( D,A,1 ) :- filho( D,A ).
@@ -272,4 +372,42 @@ descendente( D,A,G ) :-
 	descendente( X,A,N),
 	G is N+1.
 
+%--------------------------------------------------------------------
+% Extensão do predicado descendentes: Ascendente, Grau, Resultado -> {V,F}
+
+descendentes(I,G,R) :- findall(D,descendente(D,I,G),S),
+					R = S.
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado ascendente: Ascendente,Descendente,Grau -> {V,F}
+ascendente( D,A,G ) :- descendente(A,D,G).
+
+%--------------------------------------------------------------------
+% Extensão do predicado ascendentes: Descendente, Grau, Resultado -> {V,F}
+
+ascendentes(I,G,R) :- findall(A,ascendente(A,I,G),S),
+					R = S.
+
+
+%---------------------------------------------------------------------
+% Extensão do predicado descendenteAte : Descendente, Ascendente, Grau -> {V,F}
+
+descendenteAte(D,A,G) :- descendente(D,A,Z), Z=<N.
+
+
+%---------------------------------------------------------------------
+% Extensão do predicado descendentesAte : Ascendente,Grau,Resultado -> {V,F}
+
+descendentesAte(I,G,R) :- findall(D,descendenteAte(D,I,G),S), R = S.
+
+%---------------------------------------------------------------------
+% Extensão do predicado ascendenteAte : Ascendente,Descendente, Grau -> {V,F}
+
+ascendenteAte(A,D,G) :- descendenteAte(D,A,G).
+
+
+%---------------------------------------------------------------------
+% Extensão do predicado ascendentesAte : Descendente,Grau,Resultado -> {V,F}
+
+ascendentesAte(I,G,R) :- findall(A,ascendenteAte(A,I,G),S), R = S.
 
